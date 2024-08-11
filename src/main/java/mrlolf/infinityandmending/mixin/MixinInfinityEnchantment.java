2c310118d291de1c4157d330c83d56bdd6d3a490
MrLoLf <roscher2003@outlook.de>
2024-08-11 21:55:22 +0200

    updated mod for 1.20.6 compat with java 21

--- src/main/java/mrlolf/infinityandmending/mixin/MixinInfinityEnchantment.java
+++ src/main/java/mrlolf/infinityandmending/mixin/MixinInfinityEnchantment.java
@@ -1,18 +1,16 @@
 package mrlolf.infinityandmending.mixin;
 
 import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.enchantment.InfinityEnchantment;
 import net.minecraft.enchantment.MendingEnchantment;
-import net.minecraft.entity.EquipmentSlot;
 import org.spongepowered.asm.mixin.Mixin;
 
 
 @Mixin(InfinityEnchantment.class)
 public class MixinInfinityEnchantment extends Enchantment {
 
-    public MixinInfinityEnchantment(Enchantment.Rarity weight, EquipmentSlot... slotTypes) {
-        super(weight, EnchantmentTarget.BOW, slotTypes);
+    public MixinInfinityEnchantment(Properties properties) {
+        super(properties);
     }
 
     @Override
